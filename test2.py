96066be6d4715ce86efa1829ce4fe58c2a724fdd
Skyhorizon2021 <me@skyhorizon2021.com>
2023-03-07 15:00:56 -0800

    Project 2 Task 2 Update

---
+++ test2.py
@@ -0,0 +1,28 @@
+import turtle
+def getSum(num):
+    sum = 0
+    while (num != 0):
+        sum =sum + int(num % 10)
+        num = int(num/10)
+    return sum
+
+#get zipcode and replace hyphen
+zipCode = input("Enter your zip code (in ZIP+4 format): ")
+zipCode=zipCode.replace('-','')
+
+total=0
+while (n != 0):
+    return total
+checkSum= 10 ()
+print(zipCode)
+
+pen = turtle.Turtle()
+pen.hideturtle()
+pen.color("black")
+screen = turtle.Screen()
+screen.title("Zipcode Visualization")
+screen.screensize(800,500,bg="white")
+
+turtle.done()
+
+
---
+++ vignereCipher.py
@@ -0,0 +1,22 @@
+#Name: Loc Nguyen 
+#Date: 03/04/2023
+#Objective: Implement Vigenere Cipher with 2 keyboard input: plaintext and key
+
+def vignereEncrypt (key, plaintext):
+    plaintext=plaintext.lower()
+    key=key.lower()
+    plaintext=plaintext.replace(' ','')
+    keyLength=len(key)
+    cipherText=''
+    for i in range(len(plaintext)):
+        letter = plaintext[i]
+        k = key[i % keyLength]
+        cipherText = cipherText + chr(((ord(letter) - 97) + (ord(k)-97)) % 26 + 97)
+    print("Encrypted text is:",cipherText)
+    return cipherText
+
+def main():
+
+    vignereEncrypt(key,plaintext)
+
+main()
