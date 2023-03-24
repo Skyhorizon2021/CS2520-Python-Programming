9bc07dfeb5c93b5379392f979fdfcebdbf4ed670
Skyhorizon2021 <me@skyhorizon2021.com>
2023-03-24 11:51:28 -0700

    No changes

--- test.py
+++ test.py
@@ -1,82 +1,21 @@
-'''Lab 6 Task 3
-Objective:
-1. Given a tuple of names, say (“Winny”, “Ada”, “Lev”, “Kay”, “Jack”, “Sam”, “Mark”), and a list of ages, 
-e.g. [20, 18, 22, 16, 20, 18, 18], use zip feature to form a tuple of tuples, i.e.  ((“Winny”, 20), (“Ada”, 18), 
-(“Lev”, 22), (“Kay”, 16), (“Jack”, 20), (“Sam”, 18), (“Mark”, 18))
-2. From the tuple of (name, age), find the name who is the youngest as well as the average age.
-3. Sort the tuple into the descending order of the scores. If score is same, arrange the tuples in 
-alphabetical order of names. E.g. the above should be: ((‘Lev, 22), (‘Jack’, 20), (‘Winny’, 20), ...)'''
-#initilize var
-sum = 0
-count = 0
-#sort by 2nd element
-def sortTuple(tup):
-    ascSort=sorted(tup,key = lambda x: x[1])
-    return ascSort
-#reverse sort by 2nd element and alphabetically by 1st element 
-def reverseSortTuple(tup):
-    desSort=sorted(tup,key = lambda x: (-x[1],x[0]))
-    return desSort
-#initialize tuples and zip them
-t1 = ("Winny","Ada","Lev","Kay","Jack","Sam","Mark")
-t2 = (20,18,22,16,20,18,18)
-t3 = zip(t1,t2)
-sort_Tuple = sortTuple(tuple(t3))
-minPair = sort_Tuple[0]
+def firstDigit(n):
+    while n > 10:
+        n = n //10
+    else:
+        print ("First digit is",n)
 
-#assign age and name value
-minName = minPair[0]
-minAge = minPair[1]
-
-#sort in descending order
-revSort_Tuple = reverseSortTuple(sort_Tuple)
-#get sum
-for i in sort_Tuple:
-    sum += i[1]
-    count += 1
-
-#calculate average age
-avgAge = sum / count
-
-print("Tuple in descending order",revSort_Tuple)
-print("Average age is %.2f" % avgAge)
-print("The youngest is",minName,"at",minAge,"years old")
-
-#reinitialize var
-sum = 0
-count = 0
-#second test pair
-t4 = ("Psaki","Yuki","Lelouch vi Britannia","Zero","Bald Cape","Hinata","Shinichi","Doyle")
-t5 = (15,6,20,20,31,14,7,49)
-t6 = zip(t4,t5)
-sort_Tuple = sortTuple(tuple(t6))
-minPair = sort_Tuple[0]
-
-#assign age and name value
-minName = minPair[0]
-minAge = minPair[1]
-
-#sort in descending order
-revSort_Tuple = reverseSortTuple(sort_Tuple)
-#get sum
-for i in sort_Tuple:
-    sum += i[1]
-    count += 1
-
-#calculate average age
-avgAge = sum / count
-
-print("Tuple in descending order",revSort_Tuple)
-print("Average age is %.2f" % avgAge)
-print("The youngest is",minName,"at",minAge,"years old")
-'''Output
-Test(1)
-Tuple in descending order [('Lev', 22), ('Jack', 20), ('Winny', 20), ('Ada', 18), ('Mark', 18), ('Sam', 18), ('Kay', 16)]
-Average age is 18.86
-The youngest is Kay at 16 years old
-Test(2)
-Tuple in descending order [('Doyle', 49), ('Bald Cape', 31), ('Lelouch vi Britannia', 20), ('Zero', 20), ('Psaki', 15), ('Hinata', 14), ('Shinichi', 7), ('Yuki', 6)]
-Average age is 20.25
-The youngest is Yuki at 6 years old'''
-
+def lastDigit(n):
+    lastDigit = n % 10
+    print("Last digit is",lastDigit)
+def digits(n):
+    count = 0
+    while n > 1:
+        count+=1
+        n = n /10
+    else:
+        print("Number of digits is",count)
+firstDigit(1729)
+lastDigit(1729)
+digits(1729)
+    
 
--- test2.py
+++ test2.py
@@ -1,13 +1,8 @@
-def Sort_Tuple(tup): 
-  
-    # reverse = None (Sorts in Ascending order) 
-    # key is set to sort using second element of 
-    # sublist lambda has been used 
-    tup.sort(key = lambda x: x[1]) 
-    return tup 
-  
-# Driver Code 
-tup = [('rishav', 10), ('akash', 5), ('ram', 20), ('gaurav', 15)] 
-  
-# printing the sorted list of tuples
-print(Sort_Tuple(tup))
+numbers = []
+count = 0
+while count < 100:
+    numbers.append(100+count*2)
+    count+=1
+
+for value in numbers:
+    print(value)
