1dc76c7fa3f16bf562a5e3a6838fcb4ad739920a
Skyhorizon2021 <locnguyen02152021@gmail.com>
2023-03-24 19:26:20 -0700

    Task 1

--- Labs/Lab7_Lnguyen.py
+++ Labs/Lab7_Lnguyen.py
@@ -13,6 +13,73 @@ dictionary.
 tests with your own input (make sure your input paragraph having at least 50 words, or 2+ lines
 long.)'''
 
-wordList = []
+from collections import *
+#function to remove nonalphabetic characters
+def removeNonAlpha(inputStr):
+    i = 0
+    while i < len(inputStr):
+        #Find characters that doesn't fall within ASCII alphabetic range
+        if(ord(inputStr[i])<ord('A') or ord(inputStr[i])>ord('Z') or ord(inputStr[i])<ord('a') or ord(inputStr[i])>ord('z')):
+            #erase character
+            del inputStr[i]
+            i-=1
+        i+=1
+    return("".join(inputStr))
+    
+#ask user text input
 para = input("Please enter a paragraph of text:\n")
-wordList.append(para)
+#call removeNonAlpha function
+para = []
+removeNonAlpha(para)
+#split paragraph by each words and store them in a list
+wordList = para.split()
+dict1 ={}
+#add word to dictionary and count frequency 
+for word in wordList:
+    if word in dict1:
+        dict1[word] +=1
+    else:
+        dict1[word] = 1
+print("Word and Frequency List")
+#print out word and its frequency
+for word in dict1:
+    print(word,":",dict1[word],"times")
+#finding top 3 values in dictionary
+countN = Counter(dict1)
+top = countN.most_common(3)
+print("\nTop 3 word frequency in Dictionary")
+#print top 3 
+for word in top:
+    print(word[0],":",word[1],"times")
+'''Output
+Test(1)
+Please enter a paragraph of text:
+python is an easy language python is easy to learn and easy to code a lot of python modules that are easy to use go python
+Word and Frequency List
+python : 4 times
+is : 2 times
+an : 1 times
+easy : 4 times
+language : 1 times
+to : 3 times
+learn : 1 times
+and : 1 times
+code : 1 times
+a : 1 times
+lot : 1 times
+of : 1 times
+modules : 1 times
+that : 1 times
+are : 1 times
+use : 1 times
+go : 1 times
+
+Top 3 word frequency in Dictionary
+python : 4 times
+easy : 4 times
+to : 3 times
+Test(2)
+'''
+
+
+
