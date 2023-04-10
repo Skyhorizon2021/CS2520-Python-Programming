d0256172d5695e63c3544633d17f8da95ec63275
Skyhorizon2021 <me@skyhorizon2021.com>
2023-04-10 13:45:03 -0700

    Lab 8 Task 2

--- Labs/Lab8_Lnguyen.py
+++ Labs/Lab8_Lnguyen.py
@@ -1,5 +1,5 @@
 #Name: Loc Nguyen
-#Lab 7 Task 1
+#Lab 8 Task 1
 '''Objective: Use a text editor to type up a file with contents as follows and save as a .txt file.
 We can’t touch
 But we still reach out
@@ -26,3 +26,42 @@ with open("C:/Users/locng/Documents/CS2520/CS2520-Python/Labs/text.txt","r") as 
         count+=1
         if(count%2==0):
             print(line.rstrip())
+
+#Lab 8 Task 2
+#write strings one by one, i.e. write(str)
+fp=open('data1.txt', 'w')
+fp.write("hello\t")
+fp.write("how are you")
+fp.write("\n")
+fp.write("thank you ")
+fp.write("bye\n")
+fp.close()
+
+#print content
+fp=open('data1.txt','r')
+content1 = fp.read()
+print("data1.txt\n" + content1)
+fp.close()
+
+#writelines(): write a sequence, i.e. a list or a tuple into a file
+fp=open('data2.txt', 'w')
+fp.writelines(["hello\t", "how are you", "\n", "thank you ", "bye\n"])
+fp.close()
+
+#print content
+fp=open("data2.txt",'r')
+content2 = fp.read()
+print("data2.txt\n" + content2)
+fp.close()
+
+'''Output:
+data1.txt
+hello   how are you
+thank you bye
+
+data2.txt
+hello   how are you
+thank you bye
+Comparison: THey have identical output despite using different methods'''
+
+#Lab 8 Task 3
