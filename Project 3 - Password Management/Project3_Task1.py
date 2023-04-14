98dfe58c01cd336040dd46ac1612faba402c9340
Skyhorizon2021 <locnguyen02152021@gmail.com>
2023-04-13 20:58:21 -0700

    Project 3 Task 1 Completed

--- Project 3 - Password Management/Project3_Task1.py
+++ Project 3 - Password Management/Project3_Task1.py
@@ -44,11 +44,38 @@ passwords = ['sheCodes#123', 'catchAllGood1%', '@my2Choices', '123abc;;;', 'Hell
 
 #import typing to restrict what type of parameter to pass to function/method
 from typing import Dict
-
+import re
+#function to check for special characters using regex
+def specialChecker(string):
+    regex = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
+    #use search and find if there's at least 1 special char in string. False if can't find and True if found
+    if(regex.search(string)==None):
+        return False
+    else:
+        return True
+def pwdChecker(password):
+    #check for length at least 8 char
+    length = len(password)
+    #count and check for uppercase
+    uppercase = sum(1 for char in password if char.isupper())
+    #count and check for lowercase
+    lowercase = sum(1 for char in password if char.islower())
+    #count and check for number
+    digit = sum (1 for char in password if char.isdigit())
+    #check for special char
+    specialChar = specialChecker(password)
+    #if all requirement are satisfied return True, else return False
+    if(length>=8 and uppercase>0 and lowercase>0 and digit>0 and specialChar==True):
+        return True
+    else:
+        return False
+#function to log user in
 def authenticate(loginDict :Dict):
+    print("Log in below")
     #allow 3 tries
     counter = 0
     while counter<3:
+        global usernameInput
         usernameInput = input("Please enter your username: ")
         #if usrname is in dict as key, prompt password
         if usernameInput in loginDict.keys():
@@ -60,6 +87,7 @@ def authenticate(loginDict :Dict):
                 pwdInput = input("Please enter your password: ")
                 if pwdInput == realPwd:
                     print("Login Successful!")
+                    
                     return True
                 else:
                     counter +=1
@@ -67,11 +95,41 @@ def authenticate(loginDict :Dict):
             counter+=1
     else:
         return False
-        
+    
+#function to create username and password with specification
 def newUser(loginDict:Dict):
-    return None
+    print("Create a new user below")
+    usernameInput = input("Please create your username: ")
+    #check for unique username and prompt reenter if not unique
+    while(usernameInput==loginDict.keys()):
+        usernameInput = input("Username already existed.\nPlease create your username: ")
+    else:
+        pwdInput = input("Please create your password: ")
+        #while the pwdChecker return False meaning password doesn't meet requirement, the loop is True and will continue to run until requirement is met 
+        #so pwdChecker return True and "not True" = False will terminate the loop
+        while not(pwdChecker(pwdInput)):
+            pwdInput = input("Password complexity requirement not met. Length of at least 8, 1 lowercase, 1 uppercase, 1 digit, and 1 special character\nPlease create your password: ")
+        else:
+            print("Account Creation successful!")
+            loginDict.update({usernameInput:pwdInput})   
+    return usernameInput
+#function to update password
 def updatePwd(loginDict:Dict):
-    return None
+    print("Update your password below. Please log in first")
+    if authenticate(loginDict) == True:
+        pwdInput = input("Please update your password: ")
+        #while the pwdChecker return False meaning password doesn't meet requirement, the loop is True and will continue to run until requirement is met 
+        #so pwdChecker return True and "not True" = False will terminate the loop
+        while not(pwdChecker(pwdInput)):
+            pwdInput = input("Password complexity requirement not met. Length of at least 8, 1 lowercase, 1 uppercase, 1 digit, and 1 special character\nPlease update your password: ")
+        else:
+            print("Password Update Successful for User",usernameInput)
+            loginDict.update({usernameInput:pwdInput})
+            return True
+    else:
+        return False
+
+
 def main():
     #initialize user and password list
     userList = ['lyang', 'kSimon', 'danny', 'tomatcpp', 'csDept', 'CoScpp', 'broncoWins', 'ponyExp','BldgAndRooms', 'helloKitty' ]
@@ -81,10 +139,68 @@ def main():
     '''for items in loginDict.items():
         print(items[0],":",items[1])'''
         
-    #prompt user login
+    #prompt user login & print corresponding status update
     if authenticate(loginDict) == True:
         pass
     else:
-        pass
+        print("Login Unsuccessful!")
+    
+    #create a new user, if account creation is successful, it will return the username. We'll check if the username is in the dictionary, if it is, welcome the user
+    newUsername = newUser(loginDict)
+    if(newUsername in loginDict.keys()):
+        print("Welcome",newUsername+"!")
+    #update password
+    if updatePwd(loginDict) == False:
+        print("Password update unsuccessful. End of program!")
 #execute main function
 main()
+'''Output
+Test(1) - Successful login, create account, and update password
+Log in below
+Please enter your username: iwant
+Please enter your username: youtube
+Please enter your username: helloKitty
+Please enter your password: 1Startup@sdlfaksdjf
+Please enter your password: 1Startup@Starbucks
+Login Successful!
+Create a new user below      
+Please create your username: Analys
+Please create your password: opening
+Password complexity requirement not met. Length of at least 8, 1 lowercase, 1 uppercase, 1 digit, and 1 special character
+Please create your password: Urthebest263
+Password complexity requirement not met. Length of at least 8, 1 lowercase, 1 uppercase, 1 digit, and 1 special character
+Please create your password: Getthebag78@
+Account Creation successful!
+Welcome Analys!
+Update your password below. Please log in first
+Log in below
+Please enter your username: tomatcpp
+Please enter your password: John
+Please enter your password: 123abc;;;
+Login Successful!
+Please update your password: 4Begone123!
+Password Update Successful for User tomatcpp
+Test(2) - Unsuccessful login, succesful create account, unsuccessful password update
+Log in below
+Please enter your username: Analys
+Please enter your username: ponyExp
+Please enter your password: Johnl
+Please enter your password: I can't take it
+Please enter your password: python
+Login Unsuccessful!
+Create a new user below
+Please create your username: user
+Please create your password: user
+Password complexity requirement not met. Length of at least 8, 1 lowercase, 1 uppercase, 1 digit, and 1 special character
+Please create your password: I want to be 11
+Password complexity requirement not met. Length of at least 8, 1 lowercase, 1 uppercase, 1 digit, and 1 special character
+Please create your password: Python2520!
+Account Creation successful!
+Welcome user!
+Update your password below. Please log in first
+Log in below
+Please enter your username: Ucla
+Please enter your username: Cpp
+Please enter your username: Get me a 
+Password update unsuccessful. End of program!
+'''
--- test.py
+++ test.py
@@ -1,6 +1,34 @@
-t = set()
-print(type(t))
-t.update("1","2","3")
-print(t)
-for element in t:
-    print(element,end="\n")
+import re
+def specialChecker(string):
+    regex = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
+    #use search and find if there's at least 1 special char in string. False if can't find and True if found
+    if(regex.search(string)==None):
+        return False
+    else:
+        return True
+def pwdChecker(password):
+    #count and check for uppercase
+    uppercase = sum(1 for char in password if char.isupper())
+    print(uppercase)
+    #count and check for lowercase
+    lowercase = sum(1 for char in password if char.islower())
+    print(lowercase)
+    #count and check for number
+    digit = sum (1 for char in password if char.isdigit())
+    print(digit)
+    #check for special char
+    specialChar = specialChecker(password)
+    print(specialChar)
+    #if all requirement are satisfied return True, else return False
+    if(uppercase>0 and lowercase>0 and digit>0 and specialChar==True):
+        return True
+    else:
+        return False
+
+dictionary = {"John":1,"Ana":2,"Bell":3}
+for items in dictionary.items():
+    print(items[0],":",items[1])
+dictionary.update({"John":123})
+for items in dictionary.items():
+    print(items[0],":",items[1])
+    
