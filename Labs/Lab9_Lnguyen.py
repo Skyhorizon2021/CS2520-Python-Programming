0ac20498f46b7cf9ef4b4171db0494a60afd598b
Skyhorizon2021 <locnguyen02152021@gmail.com>
2023-04-19 00:15:02 -0700

    Update Questioning method

--- Labs/Lab9_Lnguyen.py
+++ Labs/Lab9_Lnguyen.py
@@ -23,6 +23,18 @@ class Question:
         self.choices = choices
         self.correctAnswer = correctAnswer
         pass
+#update score if userScore is higher than highscore
+class Score:
+    def __init__(self,userScore,highscore):
+        self.userScore = userScore
+        self.highscore = highscore
+    def updateScore(self):
+        if self.userScore > self.highscore:
+            self.highscore = self.userScore
+            print("New high score achieved!")
+            print("High Score:",self.highscore)
+        else:
+            pass
 #create questions
 def createQuestion():
     q1 = Question("What was the name of the first human in space?",{"A":"Neil Amstrong","B":"Edwin Aldrin","C":"Yuri Gagarin","D":"Victor Glover"},"C")
@@ -46,12 +58,41 @@ def createQuestion():
     q19 = Question("How many UC campuses are in the UC system?",{"A":"11","B":"9","C":"10","D":"13"},"B")
     q20 = Question("What is Cal Poly Pomona address?",{"A":"3708 W Temple Ave","B":"3801 W Temple Ave","C":"1403 W Temple Ave","D":"1803 W Temple Ave"},"B")
     
-    pass    
-#function to select random question
-def randomQuestion(questionBank):
+    return q1,q2,q3,q4,q5,q6,q7,q8,q9,q10,q11,q12,q13,q14,q15,q16,q17,q18,q19,q20    
+#function to select random question,print it, ask for user input, grade it
+def Questioning(questionBank):
+    #initialize user score and high score of 0
+    scoreBoard = Score(0,0)
+    #initalize a list containing 20 numbers corresponding to 20 questions. If a question number has been asked, removed it from the list
+    qList = [num for num in range(20)]
+    #ask users 5 random questions
+    for i in range(5):
+        #select a random number from list use that random number correspond to that question
+        qNumber = random.choice(qList)
+        #remove that number from list
+        qList.remove(qNumber)
+        #print question
+        print("Question",(i+1),"-",questionBank[qNumber].question)
+        #print out 4 choices
+        choiceDict = questionBank[qNumber].choices
+        for choice in choiceDict:
+            print(choice+".",choiceDict[choice])
+        #ask user for their answer
+        userAnswer = input("Your answer: ")
+        #compare user input with correct answer. If correct, +1 score. If not, do nothing
+        correctAnswer = questionBank[qNumber].correctAnswer
+        if userAnswer == correctAnswer:
+            scoreBoard.userScore+=1
+        else:
+            pass   
+             
+        
     pass
-
 #main function
 def main():
+    name = input("Welcome to Trivia Night! What's your name?\n")
+    print("Hello",name+".","Let's start! You'll be answering 5 questions")
     print("Please enter only the letter when selecting answer")
-    createQuestion()
+    Questioning(createQuestion())
+
+main()
