bc8f3390e6b3bc6e0a05734b3ce0ed3e9306a18a
Skyhorizon2021 <me@skyhorizon2021.com>
2023-04-21 15:39:19 -0700

    Task 1 Lab 10 Update

--- test.py
+++ test.py
@@ -1,34 +1,13 @@
-import re
-def specialChecker(string):
-    regex = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
-    #use search and find if there's at least 1 special char in string. False if can't find and True if found
-    if(regex.search(string)==None):
-        return False
-    else:
-        return True
-def pwdChecker(password):
-    #count and check for uppercase
-    uppercase = sum(1 for char in password if char.isupper())
-    print(uppercase)
-    #count and check for lowercase
-    lowercase = sum(1 for char in password if char.islower())
-    print(lowercase)
-    #count and check for number
-    digit = sum (1 for char in password if char.isdigit())
-    print(digit)
-    #check for special char
-    specialChar = specialChecker(password)
-    print(specialChar)
-    #if all requirement are satisfied return True, else return False
-    if(uppercase>0 and lowercase>0 and digit>0 and specialChar==True):
-        return True
-    else:
-        return False
+import cmath
+class Complex:
+  def __init__(self, real, imaginary):
+    self._real = real
+    self._imaginary = imaginary
 
-dictionary = {"John":1,"Ana":2,"Bell":3}
-for items in dictionary.items():
-    print(items[0],":",items[1])
-dictionary.update({"John":123})
-for items in dictionary.items():
-    print(items[0],":",items[1])
-    
+  def __add__(self,rhsValue):
+    real = self._real + rhsValue._real
+    imaginary = self._imaginary + rhsValue._imaginary
+    return Complex(real, imaginary)
+  
+sum = Complex(2,4j)
+print()
