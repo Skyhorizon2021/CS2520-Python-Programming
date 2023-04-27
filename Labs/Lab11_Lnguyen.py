e0802d58d30ad917d7f4834c2cf476cc45be775f
Skyhorizon2021 <me@skyhorizon2021.com>
2023-04-27 15:00:56 -0700

    Setup Functions

---
+++ Labs/Lab11_Lnguyen.py
@@ -0,0 +1,35 @@
+#Name: Loc Nguyen
+#Date: 04/27/2023
+'''Objective: 
+(5 pts) Write a general sort function that takes a comparison function as well as a list of values
+as parameters, then sort the list according to the comparison function provided. May use any
+sorting algorithm but do NOT use any predefined sorting function. Test the following:
+(1) Sort the integer list [5, 2, 12, 4, 9, 13, 22, 1, 6, 17] to descending order
+(2) Sort the name list [“Kate”, “Sam”, “Kate”, “Jolly”, “Alp”, “Beta”, “Alpine”, “Samuel”, “Bob”,
+“Joy”] according to alphabetical order.
+(3) Sort the tuple list of (name, count) according to name’s alphabetical order. If same name,
+then the one has higher count listed first. [(“Kate”, 3), (“Sam”, 2), (“Kate”, 5), (“Jolly”, 1),
+(“Alp”, 2), (“Beta”, 3), (“Alp”, 1), (“Alpine”, 2), (“Sam”, 4), (“Bob”, 2), (“Sam”, 3)].
+'''
+
+#general sorting algorithm using quicksort
+def generalSort(compFunc,data):
+    pass
+
+#comparison functions
+#descending order algorithm for list
+def reverseSort(values):
+    pass
+
+#ascending alpha order algo for list
+def alphaSort(values):
+    pass
+
+#sort ascending alpha with higher count for secondary criteria 
+def tupleSort(values):
+    pass
+
+def main():
+    pass
+
+main()
