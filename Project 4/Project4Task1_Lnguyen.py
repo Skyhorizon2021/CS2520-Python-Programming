d0e9de75945641b0d2a081151f8e6adbeeec7743
Skyhorizon2021 <me@skyhorizon2021.com>
2023-05-04 11:13:46 -0700

    Finished Project4 T1

---
+++ Project 4/Project4Task1_Lnguyen.py
@@ -0,0 +1,71 @@
+class Pair:
+    #constructor 
+    def __init__(self,x=0,y=0) -> None:
+        self.x = x
+        self.y = y
+    #overloading +,*, and / operator
+    def __add__(self,other):
+        return Pair(self.x+other.x,self.y+other.y)
+    def __mul__(self,other):
+        return Pair(self.x*other.x,self.y*other.y)
+    #use truediv to overload /, div will only overload // automatically
+    def __truediv__(self,other):
+        return Pair(self.x*self.y-other.x*other.y,self.x*other.y-self.y*other.y)
+    #define string method to print
+    def __str__(self) -> str:
+        return ("<"+str(self.x)+","+str(self.y)+">")
+
+def main():
+    #Test case 1
+    print("Test run 1")
+    p1 = Pair(3,2)
+    p2 = Pair(1,5)
+    p3 = Pair(4,3)
+    result1 = p1+p2
+    result2 = p1*p2
+    result3 = p1/p2
+    result4 = p1+p2*p3
+    result5 = p1*p2/p3+p1
+    #display result
+    print("p1 =",p1)
+    print("p2 =",p2)
+    print("p3 =",p3)
+    print("Result 1 =",result1)
+    print("Result 2 =",result2)
+    print("Result 3 =",result3)
+    print("Result 4 =",result4)
+    print("Result 5 =",result5)
+
+    #Test case 2
+    print("Test run 2")
+    p1 = Pair(10,2)
+    p2 = Pair(3,4)
+    p3 = Pair(9,5)
+    result1 = p1+p2
+    result2 = p1*p2
+    result3 = p1/p2
+    result4 = p1+p2*p3
+    result5 = p1*p2/p3+p1
+    #display result
+    print("p1 =",p1)
+    print("p2 =",p2)
+    print("p3 =",p3)
+    print("Result 1 =",result1)
+    print("Result 2 =",result2)
+    print("Result 3 =",result3)
+    print("Result 4 =",result4)
+    print("Result 5 =",result5)
+    
+main()
+'''Output:
+Test(1)
+p1 = <3,2>
+p2 = <1,5>
+p3 = <4,3>
+Result 1 = <4,7>
+Result 2 = <3,10>
+Result 3 = <1,5>
+Result 4 = <7,17>
+Result 5 = <21,-19>
+Test(2)
+'''
--- Project 4/Project4Task2.py
+++ Project 4/Project4Task2.py
@@ -1,10 +1,6 @@
 #Task 2
 import turtle
 
-
-screen = turtle.Turtle()
-pen = turtle.pen()
-
 class Polygon:
     def __init__(self,list) -> None:
         self.list = list
@@ -20,7 +16,20 @@ class Polygon:
 
 
 def main():
+    #setup Turtle
+    screen = turtle.Screen()
+    pen = turtle.Turtle()
+    screen.title("Polygon")
+    screen.screensize(800,500,bg="black")
+
+    pen.hideturtle()
+    pen.pencolor("white")
+
+    pen.speed(5)
+    #define private pointList object and pass argument to it
     __pointList = Polygon([(0,0),(0,5),(3,2)])
     __pointList.displaySide()
 
 main()
+
+
